baf94d407424a70a0dd30cd04e43610cf2a14efc
jhhr <jormki@gmail.com>
2024-05-21 21:36:28 +0300

    Save/read configs now applied on sync, Removed buttons (#1)
    * Added docs to saveConfigs and readConfigs
    * Update README.md with sync description

--- __init__.py
+++ __init__.py
@@ -1,23 +1,6 @@
-from aqt import mw
-from aqt.qt import QAction
+from aqt.gui_hooks import sync_will_start, media_sync_did_start_or_stop
 
 from .funcs import readConfigs, saveConfigs
 
-
-def build_action(fun, text, shortcut=None):
-    """fun -- without argument
-    text -- the text in the menu
-    """
-    action = QAction(text)
-    action.triggered.connect(lambda b: fun())
-    if shortcut:
-        action.setShortcut(shortcut)
-    return action
-
-
-readAction = build_action(readConfigs, "Read Configs")
-saveAction = build_action(saveConfigs, "Save Configs")
-
-menu_for_helper = mw.form.menuTools.addMenu("Sync Addon Configs")
-menu_for_helper.addAction(readAction)
-menu_for_helper.addAction(saveAction)
+sync_will_start.append(saveConfigs)
+media_sync_did_start_or_stop.append(readConfigs)
